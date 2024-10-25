8b99d396e2dcaf7b7032d94747e01f8f1d374420
Reid3002 <reid3002@gmail.com>
2024-10-24 22:21:59 -0300

    IMove

---
+++ Assets/Scripts/Pathfinding/IMove.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMove
+{
+    void Move(Vector3 direction);
+
+    void Look(Vector3 direction);
+
+    void Look(Transform target);
+
+    void SetPosition(Vector3 position);
+}
